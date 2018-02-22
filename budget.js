706fabaae592e637b0c2ba8e147c5f63f1d633c2
Nikolay Ab <ab.nikolay@yandex.ru>
2018-02-22 16:01:15 +0300

    Saving with confirm/Loading, global object

--- budget.js
+++ budget.js
@@ -1,38 +1,74 @@
-function updateBudget() {
-     
+const N_ENTRIES = 20;
+BudgetObj = {
+                 "temp_val" : 0,
+                 "entry" : [ ]
+                // 
+                };
+
+function EntryObj() {
+                this.imp = null;
+                this.category = null;
+                this.entry = null;
+                this.depth = null;
+                this.type = null;
+                this.val = null;
     
-    var BudgetObj = {
-                        "entry" : [ ]
-    };
+                this.isFull = function() {
+                   return 0;//( (this.val >= this.depth) && (this.type == "const") )
+                }
+           }
+
+
+//Making a table HTML
+var template = document.querySelector('#raw_tmplt')
+for (var i=0; i<N_ENTRIES; i++) {
+    var clone = template.content.cloneNode(true)
+    var input = clone.querySelectorAll('input')
+    input[0].id = "imp_" + i
+    input[0].value = i
+    input[1].id = "category_" + i
+    input[2].id = "entry_" + i
+    input[3].id = "depth_" + i
+    input[4].id = "val_" + i
+    input[4].value = 0;
+    var select = clone.querySelectorAll('select')
+    select[0].id = "type_" + i
+    template.parentNode.appendChild(clone)
+}
+
+
+function saveBudget() {    
     
-    for (let i=0; i<3; i++){ 
-        var EntryObj = {
-                        "imp" : null,
-                        "category" : null,
-                        "entry" : null,
-                        "depth" : null,
-                        "type" : null,
-                        "val" : null
-                    };
+   // alert(JSON.stringify(BudgetObj.entry[0]))
+    
+    for (let i=0; i<N_ENTRIES; i++){ 
         
-        for (column in EntryObj)
-           EntryObj[column] = document.getElementById(column + "_" + i).value 
+        //var BudgetObj.entry[i] = {};
+        var entry = new EntryObj();
         
-        if (EntryObj.imp != "")
-            BudgetObj.entry[i] = EntryObj        
+        for (column in entry) {
+            if (!(typeof entry[column] === "function"))
+               entry[column] = document.getElementById(column + "_" + i).value 
+        }
+        
+       // if (EntryObj.imp != "")
+          BudgetObj.entry[i] = entry;
+        //alert(BudgetObj.entry.length)
     }
     
-    
-    
+   // alert(JSON.stringify(BudgetObj))
     let BudgetJSON = JSON.stringify(BudgetObj)
-    alert(BudgetJSON)
     localStorage.setItem("budget.json", BudgetJSON)
-    alert("Updated and saved successfully")
+}
+
+function saveBudget_confirm(){
+    if (confirm("Are you sure? This will rewright database"))
+        saveBudget();
 }
 
 function loadBudget(){
-    var BudgetJSON = localStorage.getItem("budget.json");
-    var BudgetObj = JSON.parse(BudgetJSON);
+    let BudgetJSON = localStorage.getItem("budget.json");
+    BudgetObj = JSON.parse(BudgetJSON);
     
     //alert(JSON.stringify(BudgetObj))
     for (var i in BudgetObj.entry) {
@@ -41,3 +77,10 @@ function loadBudget(){
         }
     }
 }
+
+
+//function addIncome(){
+//    alert(BudgetObj.entry[0].isFull())
+//}
+
+
